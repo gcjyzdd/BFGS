648e95f5f250473bef9a6db4fb084f9152c0a08a
Changjie <changjieguan@gmail.com>
2018-04-29 15:53:47 +0200

    Passed the test of mpc.

--- src/main.cpp
+++ src/main.cpp
@@ -31,9 +31,49 @@
 #include<iostream>
 #include <chrono>
 #include<vector>
+#include<functional>
+#include <math.h>
+
+#include "mpc.hpp"
+#include "bfgs.hpp"
+
+using namespace std;
 
 int main()
 {
+	BFGS solver;
+	Motion_Model mpc;
 
+	mpc.m_arg.coeffs[0] = -1;
+	mpc.m_arg.coeffs[1] = 0;
+	mpc.m_arg.coeffs[2] = 0;
+
+	mpc.m_arg.x = -1;
+	mpc.m_arg.y = 10;
+	mpc.m_arg.psi = 0;
+	mpc.m_arg.v = 10;
+	mpc.m_arg.cte = polyval(mpc.m_arg.coeffs, mpc.m_arg.x) - mpc.m_arg.y;
+	mpc.m_arg.epsi = mpc.m_arg.psi - atan(mpc.m_arg.coeffs[1] + 2*mpc.m_arg.coeffs[2]);
+
+	mpc.m_setting.N = 25;
+	mpc.m_setting.m = 2;
+	mpc.m_setting.Lf = 1.17;
+	mpc.m_setting.dt = 0.05;
+	mpc.m_arg.ref_v = 40;
+
+	using std::placeholders::_1;
+	using std::placeholders::_2;
+	Cost_Fun cost_fun1 = std::bind( &Motion_Model::cost, mpc, _1 );
+	//std::function<double(VectorXd &)> cost_fun1 = std::bind( &Fun1::cost, f1, _1 );
+	Diff_Fun diff_fun1 = std::bind( &Motion_Model::diff, mpc, _1,_2 );
+
+	int iters = 50;
+	double cost;
+
+	VectorXd rst(mpc.m_setting.m * (mpc.m_setting.N - 1));
+	rst.fill(0.);
+	cost = solver.solve(cost_fun1,diff_fun1,rst);
+
+	cout<<"cost = "<<cost<<", step = "<<solver.step<<endl;
 	return 0;
 }
